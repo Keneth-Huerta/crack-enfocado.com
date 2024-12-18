dfe4800216ccdf726c7062e78a36ec1a06067bbb
driv7 <rodrigo.popoca.2007@gmail.com>
2024-12-17 23:23:14 -0600

    f

--- php/descprod.php
+++ php/descprod.php
@@ -1,21 +1,24 @@
 <link rel="stylesheet" href="estilocinco.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+<!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"> -->
 
 <div id="head" class="head row">
-
- <div class="row">
+    
+<div class="row">
     <div class="col-md-4 col-md-offset-4">
     <center>
     <img src=".\promos\simi.jpg" height="120px" width="120px">
     <p>MINI SIMIS</p>
     </center>
-
 </div>
 </div>
 </div>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
 <p style="text-align: right;">ys</p>
 
+<a href="index.php" style = "text-align: right"> INICIO</a>
+<a href="productos.php"> PRODUCTOS </a>
+<a href="contacto.php"> CONTACTO </a>
 <div class="row">
 <div class="col-md-3 col-md-offset-1"><img src=".\promos\simi.jpg" width="300px" height="300px" ></div>
 <div class="col-md-8">
