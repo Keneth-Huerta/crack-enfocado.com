d4928d33b722ebd70ab6319073303513a4fe5ab0
driv7 <rodrigo.popoca.2007@gmail.com>
2024-12-17 23:31:07 -0600

    231

--- php/descprod.php
+++ php/descprod.php
@@ -1,15 +1,14 @@
 <link rel="stylesheet" href="estilocinco.css">
-<!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"> -->
 
 <div id="head" class="head row"></div>
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
 <div class="row">
-    <p> </p>
+
+
 <div class="col-md-3 col-md-offset-1"><img src=".\promos\simi.jpg" width="300px" height="300px" ></div>
 <div class="col-md-8">
-<h1>Mini Simi :D</h1>
+<h1>Producto</h1>
 <div class="Descuento" style="color: #ff0000">
          <p><span class="label label-danger">50%</span></p>
          </div>
