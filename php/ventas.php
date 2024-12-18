7a24bca8ad01cc1283cb878ecfcf35ee9ff78438
camish17 <camilanahe@gmail.com>
2024-12-17 23:14:17 -0600

    a

--- php/ventas.php
+++ php/ventas.php
@@ -98,7 +98,7 @@
 <body>
     <section class="sales-section">
         <h1 class="sales-title">Materiales</h1>
-        <p class="sales-description">Explora la variedad de materiales cragados por los alumnos</p>
+        <p class="sales-description">Explora la variedad de materiales cargados por los alumnos</p>
 
         <div class="sales-cards">
             <?php
