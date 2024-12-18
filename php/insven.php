a255265c4280a466241d76c82d34a54106c11fdb
Alexanderdd1 <rodriguezlopezcesaralexander@gmail.com>
2024-12-18 13:40:48 -0600

    as

--- php/insven.php
+++ php/insven.php
@@ -83,11 +83,17 @@
 
 
 <?php
-// Conectar a la base de datos
-require_once 'conexion.php';
-// Consultar los productos de la base de datos
+session_start();
+// Configuración de la base de datos
+$servidor = "localhost";
+$usuarioBD = "u288355303_Keneth";
+$claveBD = "1420Genio.";
+$baseDeDatos = "u288355303_Usuarios";
+
+// Conexión a la base de datos
+$enlace = mysqli_connect($servidor, $usuarioBD, $claveBD, $baseDeDatos);
 $sql = "SELECT * FROM productos";
-$result = $conn->query($sql);
+$result = $enlace->query($sql);
 
 echo '<div class="products-container">';
 if ($result->num_rows > 0) {
@@ -104,7 +110,7 @@ if ($result->num_rows > 0) {
 }
 echo '</div>';
 
-$conn->close();
+$enlace->close();
 ?>
 
 <style>
