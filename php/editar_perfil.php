cf5bcd2699fb16fe7c85ca5601a14f6d0c67c315
Keneth Huerta <kenethissac@gmail.com>
2024-12-18 13:19:53 -0600

    css

--- php/editar_perfil.php
+++ php/editar_perfil.php
@@ -92,7 +92,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar perfil</title>
-    <link rel="stylesheet" href="css/misestilos.css">
+    <link rel="stylesheet" href="../css/misestilos.css">
 </head>
 
 <body>
