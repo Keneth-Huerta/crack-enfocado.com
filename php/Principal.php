6a3797010c84afca53f15ab6d2ac0d0867a44a3e
Miquiztli22 <adam22mpg11@gmail.com>
2024-12-18 01:26:28 -0600

    adam

--- php/Principal.php
+++ php/Principal.php
@@ -1,55 +1,70 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Plantilla de Sitio Web</title>
     <link rel="stylesheet" href="../css/estilosprin.css">
 </head>
+
 <body>
-        <nav>
-            <ul>
-                <li class= "cabeza"><a href="#">Logos</a></li>
-                <li class= "cabeza"><a href="#">Ventas</a></li>
-                <li class= "cabeza"><a href="#">Ayuda Estudiantil</a></li>
-            </ul>
-        </nav>
+    <nav>
+        <ul>
+            <li>
+                <div class="logo"><img src="../media/logoweb.jpg" alt=""></div>
+            </li>
+            <li>
+                <div class="nav-links">
+                    <a href="/ventas">Ventas</a>
+                    <a href="/ayuda-estudiantil">Ayuda Estudiantil</a>
+                    <img src="../media/user_icon_001.jpg" alt="Perfil">
+                </div>
+            </li>
+
+        </ul>
+    </nav>
 
     <main>
-        <section class="recent-content">
-            <h2>Lo más reciente...</h2>
-            <div class="content-item">
-                <img src="#" alt="Imagen de contenido">
-                <a href="#">Ver más</a>
-            </div>
-        </section>
 
-        <section class="more-posts">
-            <h2>Más publicaciones</h2>
-            <div class="post-item">
-                <img src="#" alt="Imagen de publicación">
-                <h3>Título de la publicación</h3>
-                <p>Resumen de la publicación</p>
-                <a href="#">Leer más</a>
-            </div>
-        </section>
+        <h2>Lo más reciente...</h2>
+        <div class="content-item">
+            <img src="#" alt="Imagen de contenido">
+            <a href="#">Ver más</a>
+        </div>
 
-        <section class="school-info">
-            <div class="info-item">
-                <h3>Ubicación</h3>
-                <p>Dirección de la escuela</p>
+
+
+        <h2>Más publicaciones</h2>
+        <div class="post-item">
+            <img src="#" alt="Imagen de publicación">
+            <h3>Título de la publicación</h3>
+            <p>Resumen de la publicación</p>
+            <a href="#">Leer más</a>
+        </div>
+
+
+        <div class="boton">
+            <div>
+                <a href="https://www.google.com/maps/place/Centro+de+Estudios+Cient%C3%ADficos+y+Tecnol%C3%B3gicos+N%C2%B0+3+%E2%80%9CEstanislao+Ram%C3%ADrez+Ruiz%E2%80%9D+IPN/@19.5707461,-99.021819,17z/data=!3m1!4b1!4m6!3m5!1s0x85d1f02f26af3137:0xe677294103786cf5!8m2!3d19.5707411!4d-99.0192441!16s%2Fg%2F1tdr3nhs?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D  "
+                    class="uno">Ubicación</a>
+
+
             </div>
-            <div class="info-item">
-                <h3>Oferta educativa</h3>
-                <p>Información sobre la oferta educativa</p>
+            <div>
+                <a href="https://www.cecyt3.ipn.mx/index.html#technicalCareers" class="uno">Oferta educativa</a>
             </div>
-            <div class="info-item">
-                <h3>SAES</h3>
-                <p>Información sobre el SAES</p>
+            <div>
+                <a href="https://www.saes.cecyt3.ipn.mx/" class="uno">SAES</a>
+
             </div>
-        </section>
+
+
+        </div>
+
     </main>
 
     <footer>
         <!-- Contenido del pie de página -->
     </footer>
 </body>
-</html>     
+
+</html>
