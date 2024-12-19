255c150b9a161ba633d1f25ab11d7b9d71c82904
Keneth Huerta <kenethissac@gmail.com>
2024-12-18 18:32:11 -0600

    comentarios 1

---
+++ php/agregar_comentario.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include 'conexion.php'; // Conexión a la base de datos
+
+// Verificar si el usuario está logueado
+if (!isset($_SESSION['usuario_id'])) {
+    header('Location: ../index.html'); // Redirigir si no está logueado
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Obtener los datos del formulario
+    $contenido_comentario = $_POST['contenido_comentario'];
+    $publicacion_id = $_POST['publicacion_id'];
+    $usuario_id = $_SESSION['usuario_id'];
+
+    // Insertar el comentario en la base de datos
+    $stmt = $enlace->prepare("INSERT INTO comentarios (usuario_id, publicacion_id, contenido) 
+                              VALUES (?, ?, ?)");
+    $stmt->bind_param("iis", $usuario_id, $publicacion_id, $contenido_comentario);
+    $stmt->execute();
+
+    // Redirigir al usuario a la página de publicaciones
+    header('Location: publicaciones.php');
+    exit();
+}
+?>
