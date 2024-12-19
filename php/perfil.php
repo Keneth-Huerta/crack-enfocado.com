97ed2c4b2cafcea93eae7e7e6aa094a21217377b
Keneth Huerta <kenethissac@gmail.com>
2024-12-19 15:38:06 -0600

    a ver

--- php/perfil.php
+++ php/perfil.php
@@ -88,7 +88,6 @@ $foto_portada = $perfil['foto_portada'] ?? '../media/user_icon_001.jpg';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Perfil - <?php echo htmlspecialchars($username); ?></title>
     <link rel="stylesheet" href="../css/misestilos.css">
-    <link rel="stylesheet" href="../css/publicaciones.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     
     <style>
