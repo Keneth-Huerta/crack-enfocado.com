d1943f7e37ea1f8eda7bc5cf0383a8452f60ef9c
Keneth Huerta <kenethissac@gmail.com>
2024-12-18 20:29:57 -0600

    todo

--- php/editar_publicacion.php
+++ php/editar_publicacion.php
@@ -73,7 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Publicación</title>
-    <link rel="stylesheet" href="../css/misestilos.css">
+    <link rel="stylesheet" href="../css/editarPublicacion.css">
 </head>
 <body>
     <?php include('header.php'); ?>
