eb2be518da80026e9731a58839b2e0a778a308ab
Keneth Huerta <kenethissac@gmail.com>
2025-01-08 03:01:44 -0600

    agrega estilo personalizado para enlaces de navegación en perfil.php

--- php/perfil.php
+++ php/perfil.php
@@ -112,6 +112,11 @@ $foto_portada = $perfil['foto_portada'] ?? '../media/user_icon_001.jpg';
 
     <!-- Font Awesome para iconos -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        .nav-link {
+            color: #000 !important;
+        }
+    </style>
 </head>
 
 <body>
