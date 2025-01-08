358cfa48b5f935735bfa5998695296037d917f3f
Keneth Huerta <kenethissac@gmail.com>
2025-01-07 23:29:06 -0600

    corrige el orden de los enlaces de estilos en la página de perfil para asegurar la correcta aplicación de estilos

--- php/perfil.php
+++ php/perfil.php
@@ -107,8 +107,8 @@ $foto_portada = $perfil['foto_portada'] ?? '../media/user_icon_001.jpg';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mi Perfil - <?php echo htmlspecialchars($username); ?></title>
-    <link rel="stylesheet" href="../css/misestilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/misestilos.css">
 
     <!-- Font Awesome para iconos -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
