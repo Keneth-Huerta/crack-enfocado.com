30da788e63bf85911d525fe040c7aa0f86bd2018
Keneth Huerta <kenethissac@gmail.com>
2025-01-07 21:24:33 -0600

    corrige la consulta de ventas para ordenar por idProducto en lugar de idProductos

--- php/perfil.php
+++ php/perfil.php
@@ -38,7 +38,7 @@ mysqli_stmt_execute($stmt_pub);
 $publicaciones_result = mysqli_stmt_get_result($stmt_pub);
 
 // Consultar ventas del usuario
-$ventas_query = "SELECT * FROM productos WHERE usuario_id = ? ORDER BY idProductos DESC";
+$ventas_query = "SELECT * FROM productos WHERE usuario_id = ? ORDER BY idProducto DESC";
 $stmt_ventas = mysqli_prepare($enlace, $ventas_query);
 mysqli_stmt_bind_param($stmt_ventas, "i", $usuario_ids);
 mysqli_stmt_execute($stmt_ventas);
